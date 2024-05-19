d54f72080dd712c0942a7e75bd39f22b42f2bcf2
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-19 11:01:09 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel</title>
-    <h1>HELLO LARAVEL</h1>
-    <div>{{$name}} {{$lastname}}  {{$number}}</div>
 </head>
 <body>
-    
+<h1>HELLO LARAVEL</h1>
+<div>{{$name}} {{$lastname}}  {{$number}}</div>
 </body>
 </html>
