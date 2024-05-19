9ddb309cda758abc6184b0865b8f4bc4b18b2ee1
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-19 10:59:39 +0200

    exercise completed

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        "name" => "Alfredo",
+        "lastname" => "D'Aniello",
+        "number" => "8"
+    ];
+    return view("home", $data);
 });
